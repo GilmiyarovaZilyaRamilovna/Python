0a70e8463cc3bb6620f72e876092bce4c9d497bb
Zilya Gilmiyarova <gilmiyarova.zilua.2001@mail.com>
2022-04-19 21:27:58 +0500

    19.04.2022

---
+++ 06/01.py
@@ -0,0 +1,14 @@
+a = set()
+b = set()
+c = input()
+while c != '':
+    a.add(c)
+    c = input()
+a.add(c)
+c1 = input()
+while c1 != '':
+    b.add(c1)
+    c1 = input()
+intersection = a & b
+if not intersection:
+    print('EMPTY')
