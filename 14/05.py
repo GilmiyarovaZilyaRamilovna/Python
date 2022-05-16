1741a14672f05589c39528e527755b3c3ab27b68
Zilya Gilmiyarova <gilmiyarova.zilua.2001@mail.com>
2022-05-16 21:06:53 +0500

    10.05.2022

--- 14/05.py
+++ 14/05.py
@@ -1,8 +1,14 @@
-def whoAreYouAndHello():
-    x = input()
-    while (x.isalpha() and x[0].isupper()) == False:
-        x = input()
-    print('Привет, {}!'.format(x))
-
-
-whoAreYouAndHello()
+def triangle(a, b, c):
+    max = a
+    if b > a:
+        max = b
+    if c > max:
+        max = c
+    if max >= a+b+c-max:
+        return "Это не треугольник"
+    else:
+        return "Это треугольник"
+a = int(input())
+b = int(input())
+c = int(input())
+print(triangle(a, b, c))
