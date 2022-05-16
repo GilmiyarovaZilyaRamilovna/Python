e4f3df15e4689439c1ff8a3e1c0358c49e404bf3
Zilya Gilmiyarova <gilmiyarova.zilua.2001@mail.com>
2022-05-16 22:50:46 +0500

    16.05.2022

---
+++ 16/09.py
@@ -0,0 +1,17 @@
+def hello(temp):
+    print("Hello,", temp, " !We are trying to find ur card...")
+
+
+def search_card(temp):
+    if temp in base:
+        print("We have found ur card with ", base.index(temp) + 1, " number!")
+    else:
+        print("We doesnt found ur card...")
+
+
+if __name__ == '__main__':
+    base = ["Иван", "Юлия Иванкова"]
+    hello("Иван")
+    search_card("Иван")
+    hello("Юлия Иванова")
+    search_card("Юлия Иванова")
